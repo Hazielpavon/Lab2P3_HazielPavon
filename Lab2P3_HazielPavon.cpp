3461d8c83782cdca13b5ff43d8e43b0f1d04965b
Haziel Pavon <hazielpavon93@gmail.com>
2024-01-26 15:22:10 -0600

    Terminando el segundo

--- Lab2P3_HazielPavon.cpp
+++ Lab2P3_HazielPavon.cpp
@@ -1,34 +1,83 @@
 #include <iostream>
 #include<cstdlib>
+#include <math.h>
 using namespace std;
-
-
 void ejercicio1() {
-	int arreglo[10]; 
-	int prom = 0; 
+	int arreglo[10];
+	int prom = 0;
+	double resp = 0;
 	srand((unsigned)time(NULL));
+	// consiguiendo los datos 
 	for (int i = 0; i < 10; i++)
 	{
 		int random = 100 + (rand() % 401);
-		arreglo[i] = random; 
+		arreglo[i] = random;
 		prom += arreglo[i];
 		if (i == 0) {
 			cout << "[" << arreglo[i] << ", ";
-		}else if (i > 0 && i < 9) {
-			cout << arreglo[i] << ", ";
-		}else {
-			cout  << arreglo[i] << "] " << endl;
 		}
-		
+		else if (i > 0 && i < 9) {
+			cout << arreglo[i] << ", ";
+		}
+		else {
+			cout << arreglo[i] << "] " << endl;
+		}
 	}
-	prom = prom / 10; 
-	cout << prom << endl; 
+	prom = prom / 10;
+	// for para realizar los calculos 
+	for (int i = 0; i < 10; i++) {
+		double ref = arreglo[i];
+		resp += pow(ref - prom, 2);
+	}
 
+	resp /= 10.0;
+	resp = sqrt(resp);
+	cout << "La desviacion estandar es : " << resp << endl;
+
+}
+
+void ejercicio2() {
+	int arreglo[10];
+	int numeromenor = 0;
+	srand((unsigned)time(NULL));
+
+	// llenando el arreglo y realizando lo pedido 
+	for (int i = 0; i < 10; i++)
+	{
+		int random = -10 + (rand() % 20);
+		arreglo[i] = random;
+		if (i == 0) {
+			cout << "[" << arreglo[i] << ", ";
+		}
+		else if (i > 0 && i < 9) {
+			cout << arreglo[i] << ", ";
+		}
+		else {
+			cout << arreglo[i] << "] " << endl;
+		}
+
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (arreglo[i] > 0) {
+			int ref = arreglo[i];
+			if (ref > 0 && ref < arreglo[i + 1] && ref < arreglo [i-1]) {
+				numeromenor = numeromenor++;
+			}
+			
+		}
+
+	}
+
+
+
+	cout << "Entero minimo faltante es: " << numeromenor << endl;
 }
 
 int main()
 {
-	bool seguir = true; 
+	bool seguir = true;
 	while (seguir) {
 
 		cout << "ingrese una opcion" << endl;
@@ -41,18 +90,19 @@ int main()
 
 		switch (op) {
 		case 1:
-			ejercicio1(); 
+			ejercicio1();
 			break;
 
 		case 2:
+			ejercicio2();
 			break;
 
 		case 3:
 			break;
 
 		case 4:
-			seguir = false; 
-			cout << "Saliendo..."; 
+			seguir = false;
+			cout << "Saliendo...";
 			break;
 
 		default:
